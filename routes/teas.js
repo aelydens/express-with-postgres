6dba0034deae84017c5f991cb0ede96552e2da95
Annie Lydens <aelydens@gmail.com>
2015-06-12 11:19:46 -0600

    Show teas on teas index

--- routes/teas.js
+++ routes/teas.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var connString = "postgres://@localhost/tea_raw_sql";
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var teas = [];
+  pg.connect(connString, function(err, client, done) {
+    if (err) return console.log(err);
+    var query = client.query("SELECT * FROM teas");
+
+    // streaming back rows one at a time
+    query.on('row', function(row) {
+      teas.push(row);
+    });
+    
+    // fired after last row is emitted
+    query.on('end', function() {
+      done();
+      res.render('teas/index', {teas: teas});
+    });
+  });
 });
 
 module.exports = router;
