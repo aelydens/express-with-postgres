2bf8ca6dc1efa14631571f0d62c9a9f265a1ae00
Annie Lydens <aelydens@gmail.com>
2015-06-12 11:55:29 -0600

    Add New tea form

--- routes/teas.js
+++ routes/teas.js
@@ -13,11 +13,34 @@ router.get('/', function(req, res, next) {
     query.on('row', function(row) {
       teas.push(row);
     });
-    
+
     // fired after last row is emitted
     query.on('end', function() {
       done();
       res.render('teas/index', {teas: teas});
+    });
+  });
+});
+
+
+router.get('/new', function(req, res, next) {
+  res.render('teas/new');
+});
+
+router.post('/', function(req, res, next) {
+  pg.connect(connString, function(err, client, done) {
+    if (err) return console.log(err);
+    var reorderable;
+    if (req.body['tea[reorderable]']) {
+      reorderable = true;
+    } else {
+      reorderable = false;
+    }
+
+    var query = client.query('INSERT INTO teas(name, country, price, reorderable) VALUES ($1, $2, $3, $4)', [req.body["tea[name]"], req.body["tea[country]"], req.body["tea[price]"], reorderable]);
+    query.on('end', function() {
+      done();
+      res.redirect('/teas');
     });
   });
 });
